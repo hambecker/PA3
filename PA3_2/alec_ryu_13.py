46da8a5f83f1242dca6e527d1f82d5f8e0688fb6
hambecker <abecker422@gmail.com>
2018-04-18 23:40:49 -0600

    implemented 9

--- PA3_2/alec_ryu_13.py
+++ PA3_2/alec_ryu_13.py
@@ -165,7 +165,7 @@ class SimpleSwitch13(app_manager.RyuApp):
                 elif self.current_server == 1:
                     self.current_server = 0
                 data = None
-                print("p.buffer_id = " + str(p.buffer_id))
+                # print("p.buffer_id = " + str(p.buffer_id))
                 if p.buffer_id == ofproto.OFP_NO_BUFFER:
                     data = p_copy.data
                     print("got data to send back to client")
